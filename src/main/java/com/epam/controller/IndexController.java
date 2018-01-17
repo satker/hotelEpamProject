1b702d132c7135e6dfe05ea30d65fda1ba6536f6
aslovyagin <aleksey.slovyagin95@gmail.com>
2018-01-17 18:03:26 +0300

    archetype spring app (test)

--- src/main/java/com/epam/controller/IndexController.java
+++ src/main/java/com/epam/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.epam.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,22 +10,10 @@ import java.util.Map;
 
 @Controller
 public class IndexController {
-
-    private final VisitsRepository visitsRepository;
-
-    public IndexController(VisitsRepository visitsRepository) {
-        this.visitsRepository = visitsRepository;
-    }
-
     @GetMapping("/")
     public ModelAndView index() {
         Map<String, String> model = new HashMap<>();
         model.put("name", "Alexey");
-
-        Visit visit = new Visit();
-        visit.description = String.format("Visited at %s", LocalDateTime.now());
-        visitsRepository.save(visit);
-
         return new ModelAndView("index", model);
     }
 }
