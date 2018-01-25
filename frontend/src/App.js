b117ce5dedcb2b4bb4bfbfee921ea66709efcc2d
vklimenko <vladislav_klimenko@epam.com>
2018-01-25 17:09:06 +0300

    Forms updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,46 @@
 import React, {Component} from 'react';
 import './App.css';
 
-import Form from './ListOfUsers';
+import Login from './FormLogin';
+import Register from './FormRegister';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {screen: "login"};
+        this.setScreen = this.setScreen.bind(this);
+    }
+
+    componentWillMount() {
+        this.setScreen(this.state.screen);
+    }
+
     render() {
         return (
             <div>
                 <header className="App-header">
                     <h1 className="App-title">EPAM Grand Hotel</h1>
                 </header>
-                <Form/>
+                {this.state.form}
             </div>
         );
+    }
+
+    setScreen(scr) {
+        let form = null;
+        switch (scr) {
+            case "login":
+                form = <Login setScreen={this.setScreen}/>;
+                break;
+
+            case "register":
+                form = <Register setScreen={this.setScreen}/>;
+                break;
+
+            default:
+                form = <p>Error.</p>;
+        }
+        this.setState({screen: scr, form: form});
     }
 }
 
