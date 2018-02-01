81f8f651ee6eb57508504658ccae58a21a5ba24a
aslovyagin <aleksey.slovyagin95@gmail.com>
2018-02-01 12:36:55 +0300

    cascade deleting added

--- src/main/java/com/epam/model/RoomConfirm.java
+++ src/main/java/com/epam/model/RoomConfirm.java
@@ -1,11 +1,13 @@
 package com.epam.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 
 @Entity
@@ -20,14 +22,17 @@ public class RoomConfirm {
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private User user;
 
     @OneToOne
     @JoinColumn(name = "room_request_id")
+    @JsonIgnore
     private RoomRequest request;
 
     @ManyToOne
     @JoinColumn(name = "room_id")
+    @JsonIgnore
     private Room room;
 
     @PostPersist
@@ -47,5 +52,19 @@ public class RoomConfirm {
 
     private void audit(String operation) {
         log.debug("operation to room confirm table completed {}", operation);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        RoomConfirm that = (RoomConfirm) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id);
     }
 }
--- src/main/java/com/epam/model/RoomType.java
+++ src/main/java/com/epam/model/RoomType.java
@@ -1,19 +1,27 @@
 package com.epam.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 
 @Entity
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Slf4j
+@Data
+
 public class RoomType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +29,17 @@ public class RoomType {
     @NotNull
     private String name;
     private String description;
+    @OneToMany(mappedBy = "roomType")
+    @Cascade(org.hibernate.annotations.CascadeType.DELETE)
+    @Fetch(FetchMode.SELECT)
+    @JsonIgnore
+    private Set<RoomRequest> requests = new HashSet<>();
+
+    @OneToMany(mappedBy = "roomType")
+    @Cascade(org.hibernate.annotations.CascadeType.DELETE)
+    @Fetch(FetchMode.SELECT)
+    @JsonIgnore
+    private Set<Room> rooms = new HashSet<>();
     @PostPersist
     public void onPrePersist() {
         audit("INSERT");
@@ -38,5 +57,21 @@ public class RoomType {
 
     private void audit(String operation) {
         log.debug("operation to room type table completed {}", operation);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        RoomType roomType = (RoomType) o;
+        return Objects.equals(id, roomType.id) &&
+                Objects.equals(name, roomType.name) &&
+                Objects.equals(description, roomType.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, name, description);
     }
 }
